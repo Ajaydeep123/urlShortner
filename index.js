2b43a06b036ad255a9d0a8edb36802ae014d98e0
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2023-08-30 23:15:25 +0530

    🚑 ejs for ssr

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const {connectToMongoDB} = require('./connection');
+const URL = require("./models/url");
 const urlRoute = require("./routes/url");
-const URL = require('./models/url');
+const staticRoute =  require("./routes/staticRouter");
+const path = require('path');
 const app = express();
 const PORT = 8001;
 
@@ -10,7 +12,22 @@ connectToMongoDB("mongodb://127.0.0.1:27017/short-url").then(()=>{
 }).catch((error)=>{
     console.log(error);
 })
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 app.use(express.json());
+
+
+
+// app.get("/test", async (req,res)=>{
+//     const allUrls = await URL.find({});
+//     return  res.render("home",{
+//         urls:allUrls,
+//     });
+// } );
+
 app.use("/url",urlRoute);
+app.use("/",staticRoute);
+
 
 app.listen(PORT, ()=>console.log(`Server is running on Port: ${PORT}`));
